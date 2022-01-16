1855ba26081b168c70ad3c0029cc155f6505abf1
gemmaro <gemmaro.dev@gmail.com>
2022-01-16 22:58:16 +0900

    [ refactor ] lib/jkf.rb

--- lib/jkf.rb
+++ lib/jkf.rb
@@ -10,49 +10,44 @@ module Jkf
   # raise when unsupport file type
   class FileTypeError < StandardError; end
 
-  class << self
-    # ファイルからパースします。拡張子でフォーマットの判定をします。
-    #
-    # @param [String] filename
-    #
-    # @return [String] KIF, KI2, CSA, JKF(JSON)
-    def parse_file(filename, encoding: 'Shift_JIS')
-      parser = case ::File.extname(filename)
-               when /kif/
-                 ::Jkf::Parser::Kif.new
-               when /ki2/
-                 ::Jkf::Parser::Ki2.new
-               when /csa/
-                 ::Jkf::Parser::Csa.new
-               when /jkf|json/
-                 JSON
-               else
-                 raise FileTypeError
-               end
-      str = File.read(File.expand_path(filename), encoding:).toutf8
-      parser.parse(str)
-    end
+  # ファイルからパースします。
+  #
+  # 拡張子でフォーマットの判定をします。
+  #
+  # @param [String] file_name
+  #
+  # @return [String] KIF, KI2, CSA, JKF (JSON)
+  def self.parse_file(file_name, encoding: Encoding::Shift_JIS)
+    parser = case ::File.extname(file_name)
+             when '.kif', '.kifu'
+               ::Jkf::Parser::Kif.new
+             when '.ki2', '.ki2u'
+               ::Jkf::Parser::Ki2.new
+             when '.csa'
+               ::Jkf::Parser::Csa.new
+             when '.jkf', '.json'
+               JSON
+             else
+               raise FileTypeError
+             end
+    game_record_content = File.read(File.expand_path(file_name), encoding:).toutf8
+    parser.parse(game_record_content)
+  end
 
-    # 文字列からパースします。各パーサでパースに試みて成功した場合結果を返します。
-    #
-    # @param [String] str
-    #
-    # @return [Hash] JKF
-    def parse(str)
-      parsers = [::Jkf::Parser::Kif.new, ::Jkf::Parser::Ki2.new, ::Jkf::Parser::Csa.new, JSON]
+  # 文字列からパースします。
+  #
+  # 各パーサでパースに試みて、成功した場合に結果を返します。
+  #
+  # @param [String] game_record_content
+  #
+  # @return [Hash] JKF
+  def self.parse(game_record_content)
+    parsers = [::Jkf::Parser::Kif.new, ::Jkf::Parser::Ki2.new, ::Jkf::Parser::Csa.new, JSON]
 
-      result = nil
-      parsers.each do |parser|
-        begin
-          result = parser.parse(str)
-        rescue StandardError
-          next
-        end
-        break
-      end
-      raise FileTypeError if result.nil?
-
-      result
-    end
+    parsers.map do |parser|
+      parser.parse(game_record_content)
+    rescue StandardError
+      nil
+    end.compact.first || (raise FileTypeError)
   end
 end
