a7e55c68003fdea26fb4578d345f181d73bbcc30
gemmaro <gemmaro.dev@gmail.com>
2022-01-16 21:23:50 +0900

    [ lint ] Style/DoubleNegation

--- lib/jkf/parser/kif.rb
+++ lib/jkf/parser/kif.rb
@@ -280,7 +280,7 @@ module Jkf
             s3 = match_str('成')
             s3 = nil if s3 == :failed
             @reported_pos = s0
-            s0 = { 'to' => s1, 'piece' => s2, 'promote' => !!s3 }
+            s0 = { 'to' => s1, 'piece' => s2, 'promote' => !s3.nil? }
           end
         end
         s0
